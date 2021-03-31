95002cbc8dea4facf0f405f0b696be47e5991376
Amelia Clarke <amelia.amok@gmail.com>
2021-03-30 21:41:11 -0500

    Add event logger

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,48 @@
 use std::env;
 
+use serenity::async_trait;
+use serenity::model::event::ResumedEvent;
+use serenity::model::gateway::Ready;
 use serenity::prelude::*;
+use tracing::{error, info};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
+    }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+}
 
 #[tokio::main]
 async fn main() {
     // Load environment variables from .env file.
     dotenv::dotenv().expect("Failed to load .env file");
 
+    // Initialize the logger with environemnt variables.
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to start logger");
+
     // Retrieve token from environment.
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
     // Initialize client with token.
     let mut client = Client::builder(&token)
+        .event_handler(Handler)
         .await
         .expect("Error creating client");
 
     // Start the client.
     if let Err(e) = client.start().await {
-        println!("Client error: {:?}", e);
+        error!("Client error: {:?}", e);
     }
 }
